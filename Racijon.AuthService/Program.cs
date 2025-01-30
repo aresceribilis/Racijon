2815060687d3f3641ea4e4b78c14afd70827ee93
aresceribilis <aresceribilis@gmail.com>
2025-01-30 21:12:45 +0200

    Added swagger

--- Racijon.AuthService/Program.cs
+++ Racijon.AuthService/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using Racijon.AuthService;
 using System.Text;
 
@@ -10,6 +11,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Racijon API", Version = "v1" });
+});
 
 // JWT config
 var jwtSettings = builder.Configuration.GetSection("JwtSettings").Get<JwtOptions>();
@@ -43,6 +49,12 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Racijon API v1");
+        c.RoutePrefix = string.Empty;  // Open SwaggerUI on root page
+    });
 }
 
 app.UseHttpsRedirection();
